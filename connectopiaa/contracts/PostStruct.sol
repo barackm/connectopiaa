be7f5fc9de4073eb2b4d0a6c4a0a3b78e420ec96
Barack Mukelenga <barackmukelenga100@gmail.com>
2023-06-08 18:09:56 +0200

    Update main contract name

---
+++ connectopiaa/contracts/PostStruct.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+struct PostStruct {
+    uint256 postId;
+    address author;
+    string content;
+    uint256 timestamp;
+    uint256 likes;
+    bool isHidden;
+    bool isPaidContent;
+    uint256 price;
+    address[] subscribers;
+}
